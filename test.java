a5ac9360ad7002faa28d1c10cf03877fe2562531
Christoffer Lange <clange@summit.local>
2018-12-12 14:20:08 +0100

    EL-89 Another yolo

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello, Worsld");
    }
    /**
  @wi.implements http://localhost/polarion/#/project/elibrary/workitem?id=EL-89 This looks good!!
