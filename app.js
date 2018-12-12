894fa5b103e519ed6bf8771734088763b21ed4dc
Marius Grifftun <mgrifftun@summit.local>
2018-12-12 14:54:53 +0100

    DP-476 adding connection

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var http = require("http");
+var carDashboard = require("dashboard-module");
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+carDashboard.connectAndroidPanel(function (request, response) {
+
+   response.confirm(204, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello World"
-   response.end('Hello World!\n');
+   connect.bluetooth(car, frameElement);
+   connect.bluetooth(wirelessMode, unlink);
+   
+   
 }).listen(8081);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
