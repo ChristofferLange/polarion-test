beacb0e56b4e6f8609362f368bbaa7e48e298b84
Christoffer Lange <clange@summit.local>
2018-12-12 13:11:36 +0100

    DP-394 Testing integration!

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World!\n');
 }).listen(8081);
 
 // Console will print the message
