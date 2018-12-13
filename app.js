fc5eeca8db7cde79b1385ed05a60c13787bd6031
Marius Grifftun <mgrifftun@summit.local>
2018-12-13 13:04:11 +0100

    DP-476 car dashboard modification

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ carDashboard.connectAndroidPanel(function (request, response) {
    
    connect.bluetooth(car, frameElement);
    connect.bluetooth(wirelessMode, unlink);
-   connect.car(user,interface);
+   
 
   
 
